6fd01e5fd160ec84b32cc0dd90e54e8b8cea0fb2
vpoodari <vpoodari@ucsc.edu>
2018-01-21 00:47:08 -0800

    Add files via upload
    addition of sys.argv
    cleaned up to allow options

--- hack.py
+++ hack.py
@@ -15,54 +15,59 @@ def getData():
     labels = ['VariantName', 'Freq', 'Prior']
     brca1_rows = []
     brca2_rows = []
-    with sys.stdin as inputFile:
-        inputFile.readline()
-        for line in inputFile:
-            x = line.rstrip().split(' ')
-            if x[1] == '\"BRCA1\"':
-                brca1.append( (x[2], np.float(x[3]), np.float(x[4])) )
-                brca1_rows.append(x[2])
-            else:
-                brca2.append( (x[2], np.float(x[3]), np.float(x[4])) )
-                brca2_rows.append(x[2])
+    inputFile =open(sys.argv[1], 'r')
+
+    inputFile.readline()
+    for line in inputFile:
+        x = line.rstrip().split(' ')
+        if x[1] == '\"BRCA1\"':
+            brca1.append( (x[2], np.float(x[3]), np.float(x[4])) )
+            brca1_rows.append(x[2])
+        else:
+            brca2.append( (x[2], np.float(x[3]), np.float(x[4])) )
+            brca2_rows.append(x[2])
 
 
     b2 = pd.DataFrame.from_records(brca2,  columns=labels)
     b1 = pd.DataFrame.from_records(brca1, columns=labels)
-
-    return b1,b2
+    data = sys.argv[3]
+    if data == '1':
+        return b1
+    else:
+        return b2
 #VarientName, Freq, Prior
-b1,b2 = getData()
-fig_width=7
-fig_height=7
+dframe = getData()
+fig_width=10
+fig_height=10
 plt.figure(figsize=(fig_width,fig_height))
 VariantName = '"NM_007294.3:c.-192T>C"'
-panel_width=2/7
-panel_height=2/7
-panel1=plt.axes([0.1,0.1,panel_width,panel_height])
-panel1.set_ylim(0, 1)
-panel1.set_xlim(0,1)
-panel1.set_xticks(['Freq', 'Prior'])
-
-panel1.tick_params(axis='both',which='both',\
-                   bottom='off', labelbottom='off',\
-                   left='off', labelleft='off')
-
+#VariantName = '"'+sys.argv[2] + '"'
+panel_width=3/10
+panel_height=3/10
 panel2=plt.axes([0.5,0.1,panel_width,panel_height])
-panel2.set_ylim(1, 0)
-panel2.set_xlim(0, 1)
-for i in range (0, len(b1['VariantName'])):
-    if b1['VariantName'][i] != VariantName:
-        panel2.scatter(b1['Freq'][i], b1['Prior'][i], s =2 , color ='black', linewidth=0, marker='o')
+panel2.set_ylim(0, 1)
+panel2.set_ylabel('Prior')
+panel2.set_yticks(np.arange(0,1.1, 0.1))
+panel2.set_xticks(np.arange(0,1.1, 0.1))
+panel2.set_xlabel('Freq')
+panel2.set_xlim(1, 0)
+for i in range (0, len(dframe['VariantName'])):
+    if dframe['VariantName'][i] != VariantName:
+        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], s =2 , color ='black', linewidth=0, marker='o')
     else:
-        panel1.bar(0.1, b1['Freq'][i], width = 0.3)
-        panel1.bar(0.5, b1['Prior'][i], width = 0.3)
-        color = 'red' 
-        panel2.scatter(b1['Freq'][i], b1['Prior'][i], s =13 , color =color, linewidth=0, marker='o')
+        panel1=plt.axes([0.1,0.2,panel_width,panel_height])
+        panel1.set_ylim(0, 1)
+        print([dframe['Freq'][i], dframe['Prior'][i]])
+        stuff = ['Freq', 'Prior']
+        panel1.bar([0.3, 0.7], [dframe['Freq'][i], dframe['Prior'][i]] ,  width = 0.3, color = ['blue', 'orange'], tick_label = ['Freq', 'Prior'])
+        color = 'cyan' 
+        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], s =13 , color =color, linewidth=0, marker='o')
 
 panel2.tick_params(axis='both',which='both',\
-                   bottom='off', labelbottom='off',\
-                   left='off', labelleft='off')
+                   bottom='on', labelbottom='on',\
+                   left='on', labelleft='on')
+
+
 
 
 plt.savefig('data.png', dpi = 600)
