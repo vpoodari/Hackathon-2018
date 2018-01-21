8ef7ea27e7d885ecbf37e6f31b37c24bea467d5f
vpoodari <vpoodari@ucsc.edu>
2018-01-21 11:41:38 -0800

    Add files via upload

--- hack.py
+++ hack.py
@@ -1,6 +1,8 @@
-#Authors: Vinay C. Poodari & Dixon Hofflet 
+#Authors: Vinay C. Poodari, Dixon Hofflet, Justin Collet, Andrew Blair 
 #This program was designed for the parsing of a BRCA data held in a TSV file and plot the data into an easy to understand format. 
 #This project is for the MLH 2018 Hackathon held at UCSC.
+#run with  python hack.py BRCA_variant_data\ \(1\).tsv "NM_007294.3:c.-192T>C" 1 uses sys.argv, "Varient name" #gene number
+
 import sys
 import matplotlib.pyplot as plt
 import matplotlib.patches as mplpatches
@@ -8,15 +10,16 @@ import numpy as np
 import pandas as pd
 import numpy as np
 
-
 def getData():
+    """
+    This method is used to get data from the TSV file. 
+    """
     brca1 = []
     brca2 = []
     labels = ['VariantName', 'Freq', 'Prior']
     brca1_rows = []
     brca2_rows = []
     inputFile =open(sys.argv[1], 'r')
-
     inputFile.readline()
     for line in inputFile:
         x = line.rstrip().split(' ')
@@ -30,6 +33,7 @@ def getData():
 
     b2 = pd.DataFrame.from_records(brca2,  columns=labels)
     b1 = pd.DataFrame.from_records(brca1, columns=labels)
+    #returns certain data frame 
     data = sys.argv[3]
     if data == '1':
         return b1
@@ -37,37 +41,38 @@ def getData():
         return b2
 #VarientName, Freq, Prior
 dframe = getData()
-fig_width=10
-fig_height=10
+#deciding figure size
+fig_width=12
+fig_height=12
 plt.figure(figsize=(fig_width,fig_height))
-VariantName = '"NM_007294.3:c.-192T>C"'
-#VariantName = '"'+sys.argv[2] + '"'
-panel_width=3/10
-panel_height=3/10
-panel2=plt.axes([0.5,0.1,panel_width,panel_height])
+VariantName = '"'+sys.argv[2]+'"'
+#setting panel heights 
+panel_width=4/10
+panel_height=4/10
+#setting panel sizes
+panel2=plt.axes([0.3,0.55,panel_width,panel_height])
 panel2.set_ylim(0, 1)
 panel2.set_ylabel('Prior')
 panel2.set_yticks(np.arange(0,1.1, 0.1))
 panel2.set_xticks(np.arange(0,1.1, 0.1))
 panel2.set_xlabel('Freq')
 panel2.set_xlim(1, 0)
+panel2.set_title("Prior vs Freq")
 for i in range (0, len(dframe['VariantName'])):
     if dframe['VariantName'][i] != VariantName:
-        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], s =2 , color ='black', linewidth=0, marker='o')
+        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], s =9 , color ='black', linewidth=0, marker='o')
     else:
-        panel1=plt.axes([0.1,0.2,panel_width,panel_height])
+        panel1=plt.axes([0.3,0.05,panel_width,panel_height])
+        panel1.set_title("Your Varient")
         panel1.set_ylim(0, 1)
-        print([dframe['Freq'][i], dframe['Prior'][i]])
         stuff = ['Freq', 'Prior']
-        panel1.bar([0.3, 0.7], [dframe['Freq'][i], dframe['Prior'][i]] ,  width = 0.3, color = ['blue', 'orange'], tick_label = ['Freq', 'Prior'])
-        color = 'cyan' 
-        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], s =13 , color =color, linewidth=0, marker='o')
+        panel1.bar([0.3, 0.7], [dframe['Freq'][i], dframe['Prior'][i]] ,  width = 0.3, color = ['purple', 'pink'], tick_label = ['Freq: '+str(round(dframe['Freq'][i],2)), \
+            'Prior '+ str(round(dframe['Prior'][i],2))])
+        color = 'red'
+        panel2.scatter(dframe['Freq'][i], dframe['Prior'][i], color =color, linewidth=0, marker= 'X')
 
 panel2.tick_params(axis='both',which='both',\
                    bottom='on', labelbottom='on',\
                    left='on', labelleft='on')
-
-
-
 
 plt.savefig('data.png', dpi = 600)
